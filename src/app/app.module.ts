ed62a3ecf98d8e66523b537830967bad2822ef7c
Akash Sinha <Akash_Sinha1@dell.com>
2021-10-13 17:29:15 +0530

    Added basic topbar code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { TopbarComponent } from './header/topbar/topbar.component';
+import { NavbarComponent } from './header/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    TopbarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
