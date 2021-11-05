16e721d7e93cba5b41e90de74eb9f9b53bc8677b
Bharath <Bharath_kumar_Ds@Dell.com>
2021-11-05 23:02:12 +0530

    Addressed review comments

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,48 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  shopDepartment: string = "Shop departments";
-  shippingInfo: string = "Account & shipping info";
-  aboutUs: string = "About us";
-
   public shopDepartments = [
-    { id: "1",name: "Sneakers & Athletic"},
-    { id: "2",name: "Athletic Apparelc"},
-    { id: "3",name: "Sandals"},
-    { id: "4",name: "Jeans"},
-    { id: "5",name: "Shirts & Tops"},
-    { id: "6",name: "Shorts"},
-    { id: "7",name: "T-Shirts"},
-    { id: "8",name: "Swimwear"},
-    { id: "9",name: "Clogs & Mules"},
-    { id: "10",name: "Bags & Wallets"},
-    { id: "11",name: "Accessories"},
-    { id: "12",name: "Sunglasses & Eyewear"},
-    { id: "13",name: "Watches"},
+    { name: "Sneakers & Athletic"},
+    { name: "Athletic Apparelc"},
+    { name: "Sandals"},
+    { name: "Jeans"},
+    { name: "Shirts & Tops"},
+    { name: "Shorts"},
+    { name: "T-Shirts"},
+    { name: "Swimwear"},
+    { name: "Clogs & Mules"},
+    { name: "Bags & Wallets"},
+    { name: "Accessories"},
+    { name: "Sunglasses & Eyewear"},
+    { name: "Watches"},
   ];
 
   public shippingTabs = [
-    { id: "1",name: "Your account"},
-    { id: "2",name: "Shipping rates & policies"},
-    { id: "3",name: "Refunds & replacements"},
-    { id: "4",name: "Order tracking"},
-    { id: "5",name: "Delivery info"},
-    { id: "6",name: "Taxes & fees"},
+    { name: "Your account"},
+    { name: "Shipping rates & policies"},
+    { name: "Refunds & replacements"},
+    { name: "Order tracking"},
+    { name: "Delivery info"},
+    { name: "Taxes & fees"},
   ];
 
   public aboutKart = [
-    { id: "1", name: "About company"},
-    { id: "2", name: "Our team"},
-    { id: "3", name: "Careers"},
-    { id: "4", name: "News"},
+    { name: "About company"},
+    { name: "Our team"},
+    { name: "Careers"},
+    { name: "News"},
   ];
 
   public footerLabels = [
-    { name: "Fast and free delivery", content: "Free delivery for all orders over INR 200", className: "bi bi-cart-check-fill text-primary"},
+    { name: "Fast and free delivery", content: "Free delivery for all orders over ₹200", className: "bi bi-cart-check-fill text-primary"},
     { name: "Money back guarantee", content: "We return money within 30 days", className: "bi bi-currency-exchange text-primary"},
     { name: "24/7 customer support", content: "Friendly 24/7 customer support", className: "bi bi-headset text-primary"},
     { name: "Secure online payment", content: "We possess SSL / Secure сertificate", className: "bi bi-key-fill text-primary"},
   ];
+
+  trackByShopDeptName(shopDepartment: any): string {
+      return shopDepartment.name;
+  }
+
+  trackByshipTabName(shipInfo: any): string {
+    return shipInfo.name;
+  }
+
+  trackByaboutKartName(about: any): string {
+    return about.name;
+  }
 
   constructor() { }
 
