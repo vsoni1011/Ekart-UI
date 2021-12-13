13029b7bb0b077c09ea1224ddc962c4785e308a2
vaibhav soni <vaibhav_soni1@dell.com>
2021-12-13 08:47:20 +0530

    Changes

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Color } from '../../catalog/catalog.model';
+import { AdminService } from './../admin.service';
+import { Category } from './../product.model';
 
 @Component({
   selector: 'product-form',
   templateUrl: './product-form.component.html',
-  styleUrls: ['./product-form.component.scss']
+  styleUrls: ['./product-form.component.scss', './../../catalog/filter/filter.component.scss']
 })
 export class ProductFormComponent implements OnInit {
+  categoryList: Observable<Category[]>;
+  colors: Color;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onAdd(data: object) {
+    console.log(data);
+  }
+  // tslint:disable-next-line
+  trackById(index: number, data: any) {
+    return data.id;
+  }
+  getColors(): string[] {
+    return [ 'BLACK', 'BLUE', 'GREEN', 'PINK', 'RED', 'WHITE', 'YELLOW'];
+  }
 }
